21998931e1b8062e6157401321a2c84706cb7a8b
Ivan Lezcano <gabrielivanlezcano@gmail.com>
2024-05-13 03:21:07 -0300

    primer version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function saludar(texto:string){
+  alert(texto)
+
+}
+
 function App() {
+  const [seconds, setSeconds] = useState(7);
+  const [minutes, setMinutes] = useState(25);
+  const [isRunning, setIsRunning] = useState(false);
+
+  
+  useEffect(() => {
+  let interval:any
+  let minuteinterval:any
+  
+  if(isRunning){
+
+     interval = setInterval(()=>setSeconds(prevSeconds=>{
+      if(prevSeconds===0){
+        setMinutes(minute=>minute-1)
+        debugger
+        return 59 }   
+      return prevSeconds-1})
+        ,1000
+      );}
+     
+      return ()=>clearInterval(interval)
+  }, [seconds,isRunning]);
+
+
+ 
+
+  const handleclick = (event:any) => {
+    if(event.currentTarget.innerText ==="INICIO"){
+     
+      setIsRunning(true)
+       }
+  else if(event.currentTarget.innerText ==="FIN"){
+    setIsRunning(false)
+    setSeconds(59)
+  saludar("Chao")}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <p>{`${minutes}:${seconds}`}</p>
+   
+    <button onClick={handleclick} type='button'>INICIO</button>
+
+    <button onClick={handleclick} type='button'>FIN</button>
+    </>
   );
 }
 
